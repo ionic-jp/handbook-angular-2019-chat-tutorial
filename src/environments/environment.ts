0214b9e342d8c13aa774c7ecbb632ad010379dbc
榊原昌彦 <sakakibara@rdlabo.jp>
2019-09-11 10:28:47 +0900

    STEP1　Firebaseのアカウント開設とIonicへの設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBQflsX1IQaUL675Th3D6X1cISp2iLUvTs',
+    authDomain: 'ionic-chat-2d906.firebaseapp.com',
+    databaseURL: 'https://ionic-chat-2d906.firebaseio.com',
+    projectId: 'ionic-chat-2d906',
+    storageBucket: '',
+    messagingSenderId: '745785578159',
+    appId: '1:745785578159:web:887dd88d812ad33fa5ba5a'
+  }
 };
 
 /*
