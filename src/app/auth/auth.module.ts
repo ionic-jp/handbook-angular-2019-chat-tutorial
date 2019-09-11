3cc4179c4e3b3197dcb02167f788e76efae84593
榊原昌彦 <sakakibara@rdlabo.jp>
2019-09-11 10:40:53 +0900

    STEP2　認証ページモックの作成

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { SignupPage } from './signup/signup.page';
+import { SigninPage } from './signin/signin.page';
+
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupPage
+  },
+  {
+    path: 'signin',
+    component: SigninPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [SignupPage, SigninPage]
+})
+export class AuthModule {}
