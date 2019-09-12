80820b6aea2fb720284671f8510611008b27f40b
榊原昌彦 <sakakibara@rdlabo.jp>
2019-09-12 21:21:42 +0900

    STEP6　チャットのモックアップ作成

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ProfilePage } from '../shared/profile/profile.page';
+import { AuthService } from '../auth/auth.service';
+import { FirestoreService } from '../shared/firestore.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,16 +10,20 @@ import { ProfilePage } from '../shared/profile/profile.page';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
-
+  message = '';
   constructor(
     public modalController: ModalController,
+    public auth: AuthService,
+    public firestore: FirestoreService,
   ) {}
 
   async ngOnInit() {
-    const modal = await this.modalController.create({
-      component: ProfilePage,
-    });
-    await modal.present();
+    const user = await this.firestore.userInit(this.auth.getUserId());
+    if (!user.displayName) {
+      const modal = await this.modalController.create({
+        component: ProfilePage,
+      });
+      await modal.present();
+    }
   }
-
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { ModalController } from '@ionic/angular';
+import { ProfilePage } from '../shared/profile/profile.page';
 
 @Component({
   selector: 'app-tab2',
@@ -8,7 +10,17 @@ import { AuthService } from '../auth/auth.service';
 })
 export class Tab2Page {
 
-  constructor(public auth: AuthService) {}
+  constructor(
+    public auth: AuthService,
+    public modalController: ModalController
+  ) {}
+
+  async openProfile() {
+    const modal = await this.modalController.create({
+      component: ProfilePage,
+    });
+    modal.present();
+  }
 
   signOut() {
     this.auth.authSignOut();
