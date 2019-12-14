f142b33b61807d90378e8074e477c4bf94bd11cc
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2019-12-14 11:24:21 +0900

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBQflsX1IQaUL675Th3D6X1cISp2iLUvTs',
-    authDomain: 'ionic-chat-2d906.firebaseapp.com',
-    databaseURL: 'https://ionic-chat-2d906.firebaseio.com',
-    projectId: 'ionic-chat-2d906',
+    apiKey: '******',
+    authDomain: '******',
+    databaseURL: '******',
+    projectId: '******',
     storageBucket: '',
-    messagingSenderId: '745785578159',
-    appId: '1:745785578159:web:887dd88d812ad33fa5ba5a'
+    messagingSenderId: '******',
+    appId: '******',
   }
 };
